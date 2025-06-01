add0e496a720f3dd957079fd29882bad527636f2
Annie Chi <elmo1419141@gmail.com>
2025-06-01 23:23:53 +0800

    feat: display different ways to update the cache after deleting a todo

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -5,8 +5,36 @@ import ToDoItem from "./ToDoItem";
 const TodoList = () => {
   const { loading, error, data } = useQuery(GET_TODOS);
 
+  // No need to update the cache manually
   const [updateTodo] = useMutation(UPDATE_TODO);
-  const [deleteTodo] = useMutation(DELETE_TODO);
+
+  /**
+   * Different ways to update the cache after deleting a todo
+   * 1. refetchQueries
+   * 2. writeQuery
+   * 3. updateQuery
+   */
+
+  // 1. Use refetchQueries to update the cache
+  // const [deleteTodo] = useMutation(DELETE_TODO, {
+  //   refetchQueries: [{ query: GET_TODOS }],
+  // });
+
+  // 2. Use writeQuery to update the cache
+  // const [deleteTodo] = useMutation(DELETE_TODO, {
+  //   writeQuery: { query: GET_TODOS, data: { todos: data.todos.filter((todo) => todo.id !== deleteTodo.id) } },
+  // });
+
+  // 3. Use updateQuery to update the cache
+  const [deleteTodo] = useMutation(DELETE_TODO, {
+    update: (cache, { data: { deleteTodo } }) => {
+      cache.updateQuery({ query: GET_TODOS }, (data) => {
+        return {
+          todos: data.todos.filter((todo) => todo.id !== deleteTodo.id),
+        };
+      });
+    },
+  });
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
